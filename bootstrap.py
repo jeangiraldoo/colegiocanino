049c450f2a1ad1abb10351e312ee5d0e7fd91c3f
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-19 00:02:49 -0500

    chore: add dev environment setup script

---
+++ bootstrap.py
@@ -0,0 +1,30 @@
+import os
+import platform
+import subprocess
+from pathlib import Path
+
+VENV_PATH = Path("venv")  # Assuming the script is executed at the repository root
+
+OS_NAME = platform.system()
+SHELL_BIN_PATH = os.environ.get("SHELL")
+
+if SHELL_BIN_PATH:
+	shell_name = Path(SHELL_BIN_PATH).name
+else:
+	shell_name = "powershell"
+
+match OS_NAME:
+	case "Windows":
+		pip_path = VENV_PATH / "Scripts" / "pip.exe"
+		python_cmd = "python"
+	case _:
+		pip_path = VENV_PATH / "bin" / "pip"
+		python_cmd = "python3"
+
+if not VENV_PATH.exists():
+	subprocess.run([python_cmd, "-m", "venv", str(VENV_PATH)], check=True)
+
+subprocess.run(
+	[pip_path, "install", "-e", ".[dev]"],
+	check=True,
+)
