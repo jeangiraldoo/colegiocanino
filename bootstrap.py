ee164fdc60a35cd5bce010ed17781bf888446573
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-19 11:25:13 -0500

    chore(bootstrap.py): split backend and frontend logic into functions for readability

--- bootstrap.py
+++ bootstrap.py
@@ -8,20 +8,28 @@ VENV_PATH = Path("venv")  # Assuming the script is executed at the repository ro
 OS_NAME = platform.system()
 SHELL_BIN_PATH = os.environ.get("SHELL")
 
-match OS_NAME:
-	case "Windows":
-		pip_path = VENV_PATH / "Scripts" / "pip.exe"
-		python_cmd = "python"
-	case _:
-		pip_path = VENV_PATH / "bin" / "pip"
-		python_cmd = "python3"
 
-if not VENV_PATH.exists():
-	subprocess.run([python_cmd, "-m", "venv", str(VENV_PATH)], check=True)
+def setup_backend():
+	match OS_NAME:
+		case "Windows":
+			pip_path = VENV_PATH / "Scripts" / "pip.exe"
+			python_cmd = "python"
+		case _:
+			pip_path = VENV_PATH / "bin" / "pip"
+			python_cmd = "python3"
 
-subprocess.run(
-	[pip_path, "install", "-e", ".[dev]"],
-	check=True,
-)
+	if not VENV_PATH.exists():
+		subprocess.run([python_cmd, "-m", "venv", str(VENV_PATH)], check=True)
 
-subprocess.run(["npm", "install"], cwd="./client/")
+	subprocess.run(
+		[pip_path, "install", "-e", ".[dev]"],
+		check=True,
+	)
+
+
+def setup_frontend():
+	subprocess.run(["npm", "install"], cwd="./client/")
+
+
+setup_backend()
+setup_frontend()
