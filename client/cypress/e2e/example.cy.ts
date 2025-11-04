883934da1be55409b9137e29ff42c9b25c7904eb
Manuel Rodriguez <juan.rodriguez.o@correounivalle.edu.co>
2025-11-04 15:23:39 -0500

    feat: added randomized username in the register-any-user test

--- client/cypress/e2e/example.cy.ts
+++ client/cypress/e2e/example.cy.ts
@@ -1,6 +1,6 @@
 describe("Landing page", () => {
 	it("should load successfully", () => {
-		cy.visit("/")
-		cy.contains("Página de inicio (FUTURA LANDING)")
-	})
-})
+		cy.visit("/");
+		cy.contains("Página de inicio (FUTURA LANDING)");
+	});
+});
